c98a4573d152e25b12b075bafa62eb7883ce9545
t3tra <admin@t3tra.net>
2024-10-08 01:00:45 +0900

    Update main.rs
    ちょっと堅牢にしてリファクタリング

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,43 @@
 use std::fs;
+use std::io::{self, Write};
 
-fn main() {
+fn main() -> io::Result<()> {
     let mut word = String::new();
     let mut ls = String::new();
-    println!("Please input contest name (Ex: (ABC123)) >");
-    std::io::stdin().read_line(&mut word).ok();
-    println!("A..");
-    std::io::stdin().read_line(&mut ls).ok();
-    let _ = fs::create_dir(word.clone());
+
+    print!("Please input contest name (Ex: ABC123) > ");
+    io::stdout().flush()?;
+    io::stdin().read_line(&mut word)?;
+    let word = word.trim();
+
+    print!("A.. ");
+    io::stdout().flush()?; 
+    io::stdin().read_line(&mut ls)?;
+
+    match fs::create_dir(word) {
+        Ok(_) => println!("Directory '{}' created successfully.", word),
+        Err(e) => eprintln!("Failed to create directory '{}': {}", word, e),
+    }
+
     for i in 0..7 {
         if let Some(character) = char::from_u32(i + 65) {
-        let _ = fs::write(word.clone() + "/" + &character.to_string() + ".cpp", "#include <iostream>\n#include <bits/c++io.h>\n#include <string>\nusing namespace std;\n\nint main() {\n\t\n}");
-        println!("{}" , "Created ".to_owned() + &character.to_string() + ".cpp");
+            let file_name = format!("{}/{}.cpp", word, character);
+            let file_content = r#"#include <iostream>
+#include <bits/c++io.h>
+#include <string>
+using namespace std;
+
+int main() {
+    
+}"#;
+
+            match fs::write(&file_name, file_content) {
+                Ok(_) => println!("Created {}.cpp", character),
+                Err(e) => eprintln!("Failed to create {}.cpp: {}", character, e),
+            }
         }
     }
-    println!("{}", "All Completed!");
+
+    println!("All Completed!");
+    Ok(())
 }
